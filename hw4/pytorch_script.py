d2db567a895eb7ed6b23bd275ac580e90233d801
David Caldwell <djcald@cs.washington.edu>
2017-11-29 18:45:53 -0800

    working on neural nets

--- hw4/pytorch_script.py
+++ hw4/pytorch_script.py
@@ -19,16 +19,6 @@ sns.set()
 sns.set_style("white")
 %matplotlib inline
 ########################################################################
-# %%
-
-### define functions
-
-# function
-
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 ### load in data
 
@@ -43,29 +33,16 @@ transform = transforms.Compose(
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=False, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
                                           shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=False, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+testloader = torch.utils.data.DataLoader(testset, batch_size=64,
                                          shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-########################################################################
-# Let us show some of the training images, for fun.
-# %%
-
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 ########################################################################
 # 2. Define a Convolution Neural Network
@@ -104,30 +81,20 @@ class Net_conv(nn.Module):
         self.conv1 = nn.Conv2d(3,M,p)
         self.pool = nn.MaxPool2d(N,N)
         self.fc1 = nn.Linear(int((M*((33-p)/N)**2)),int(10))
-
+        self.M = M
+        self.p = p
+        self.N = N
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = x.view(-1,int((M*((33-p)/N)**2)))
+        x = x.view(-1,int((self.M*((33-self.p)/self.N)**2)))
         x = self.fc1(x)
         return x
 
+cuda_avail = torch.cuda.is_available
 
-net_simple = Net_simple()
-
-M = 200
-net_hidden = Net_hidden(M)
-
-M = 100
-p = 5
-N = 14
-net_conv = Net_conv(M,p,N)
-
-net_vec = [net_simple,net_hidden,net_conv]
-#for idx, m in enumerate(net.modules()):
-#    print(idx, '->', m)
-
-########################################################################
+# %%
+# CROSS VALIDATE!!!
 # 4. Train the network
 # ^^^^^^^^^^^^^^^^^^^^
 #
@@ -136,29 +103,36 @@ net_vec = [net_simple,net_hidden,net_conv]
 # network and optimize
 # %%
 
+# initialize number of epochs to train for
+num_epochs = 3
+net_vec = [net_conv]
 for net in net_vec:
+
+    print('Beginning network {}'.format(net))
 
 ########################################################################
 # 3. Define a Loss function and optimizer
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Let's use a Classification Cross-Entropy loss and SGD with momentum
 
-# %%
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
-
-    for epoch_ind,epoch in enumerate(range(5)):  # loop over the dataset multiple times
+    iteration_vec = []
+    train_accuracy_vec = []
+    test_accuracy_vec = []
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
 
         running_loss = 0.0
-        iteration_vec = []
-        train_accuracy_vec = []
-        test_accuracy_vec = []
+
         for i, data in enumerate(trainloader, 0):
             # get the inputs
             inputs, labels = data
 
             # wrap them in Variable
-            inputs, labels = Variable(inputs), Variable(labels)
+            if cuda_avail:
+                inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+            else:
+                inputs, labels = Variable(inputs), Variable(labels)
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -170,7 +144,7 @@ for net in net_vec:
             optimizer.step()
 
             # print statistics
-            runningStats = False
+            runningStats = True
             if runningStats:
                 running_loss += loss.data[0]
                 if i % 2000 == 1999:    # print every 2000 mini-batches
@@ -179,18 +153,170 @@ for net in net_vec:
                     running_loss = 0.0
 
             # train classification accuracy
-        correct = 0
-        total = 0
-        print('epoch {}'.format(epoch_ind))
-        for data in trainloader:
+        print('epoch {}'.format(epoch+1))
+
+        for data in testloader:
             images, labels = data
-            outputs = net(Variable(images))
+            if cuda_avail:
+                images = Variable(images.cuda())
+                labels = labels.cpu()
+            else:
+                images = Variable(images)
+
+            outputs = net(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
-            correct += (predicted == labels).sum()
+            if cuda_avail:
+                correct += (predicted.cpu() == labels).sum()
+
+            else:
+                correct += (predicted == labels).sum()
+
+        test_accuracy_vec.append(100 * correct / total)
+        iteration_vec.append(epoch+1)
+
+
+        print('Accuracy of the network on the test images: %d %%' % (
+            100 * correct / total))
+        #
+        # class_correct = list(0. for i in range(10))
+        # class_total = list(0. for i in range(10))
+        # for data in testloader:
+        #     images, labels = data
+        #     outputs = net(Variable(images))
+        #     _, predicted = torch.max(outputs.data, 1)
+        #     c = (predicted == labels).squeeze()
+        #     for i in range(4):
+        #         label = labels[i]
+        #         class_correct[label] += c[i]
+        #         class_total[label] += 1
+        #
+        #
+        # for i in range(10):
+        #     print('Accuracy of %5s : %2d %%' % (
+        #         classes[i], 100 * class_correct[i] / class_total[i]))
+
+
+    print('Finished with network {}'.format(net))
+
+    plt.figure(dpi=600)
+    plt.xlabel('iteration')
+    plt.ylabel('Accuracy')
+    plt.plot(iteration_vec,test_accuracy_vec,label='testing')
+    plt.title('{}'.format(net))
+    plt.legend()
+
+####################################################################
+
+# train and test
+
+net_simple = Net_simple()
+if cuda_avail:
+    net_simple.cuda()
+
+M_hidden = 200
+net_hidden = Net_hidden(M_hidden)
+if cuda_avail:
+    net_hidden.cuda()
+
+M_conv = 100
+p_conv = 5
+N_conv = 14
+net_conv = Net_conv(M_conv,p_conv,N_conv)
+if cuda_avail:
+    net_conv.cuda()
+
+
+net_vec = [net_simple,net_hidden,net_conv]
+for net in net_vec:
+    print('Network {}'.format(net))
+    for idx, m in enumerate(net.modules()):
+        print(idx, '->', m)
+    print('\n')
+
+########################################################################
+# 4. Train the network
+# ^^^^^^^^^^^^^^^^^^^^
+#
+# This is when things start to get interesting.
+# We simply have to loop over our data iterator, and feed the inputs to the
+# network and optimize
+# %%
+
+# initialize number of epochs to train for
+num_epochs = 3
+net_vec = [net_conv]
+for net in net_vec:
+
+    print('Beginning network {}'.format(net))
+
+########################################################################
+# 3. Define a Loss function and optimizer
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Let's use a Classification Cross-Entropy loss and SGD with momentum
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+    iteration_vec = []
+    train_accuracy_vec = []
+    test_accuracy_vec = []
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data
+
+            # wrap them in Variable
+            if cuda_avail:
+                inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+            else:
+                inputs, labels = Variable(inputs), Variable(labels)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            runningStats = True
+            if runningStats:
+                running_loss += loss.data[0]
+                if i % 2000 == 1999:    # print every 2000 mini-batches
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, i + 1, running_loss / 2000))
+                    running_loss = 0.0
+
+            # train classification accuracy
+        print('epoch {}'.format(epoch+1))
+
+        correct = 0
+        total = 0
+        for data in trainloader:
+            images, labels = data
+            if cuda_avail:
+                images = Variable(images.cuda())
+                labels = labels.cpu()
+            else:
+                images = Variable(images)
+
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            if cuda_avail:
+                correct += (predicted.cpu() == labels).sum()
+
+            else:
+                correct += (predicted == labels).sum()
+
 
         train_accuracy_vec.append(100 * correct / total)
-        print('Accuracy of the network on the 10000 train images: %d %%' % (
+        print('Accuracy of the network on the train images: %d %%' % (
             100 * correct / total))
 
         # test classification accuracy
@@ -198,156 +324,56 @@ for net in net_vec:
         total = 0
         for data in testloader:
             images, labels = data
-            outputs = net(Variable(images))
+            if cuda_avail:
+                images = Variable(images.cuda())
+                labels = labels.cpu()
+            else:
+                images = Variable(images)
+
+            outputs = net(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
-            correct += (predicted == labels).sum()
+            if cuda_avail:
+                correct += (predicted.cpu() == labels).sum()
+
+            else:
+                correct += (predicted == labels).sum()
 
         test_accuracy_vec.append(100 * correct / total)
-        iteration_vec.append(epoch_ind)
+        iteration_vec.append(epoch+1)
 
 
-        print('Accuracy of the network on the 10000 test images: %d %%' % (
+        print('Accuracy of the network on the test images: %d %%' % (
             100 * correct / total))
+        #
+        # class_correct = list(0. for i in range(10))
+        # class_total = list(0. for i in range(10))
+        # for data in testloader:
+        #     images, labels = data
+        #     outputs = net(Variable(images))
+        #     _, predicted = torch.max(outputs.data, 1)
+        #     c = (predicted == labels).squeeze()
+        #     for i in range(4):
+        #         label = labels[i]
+        #         class_correct[label] += c[i]
+        #         class_total[label] += 1
+        #
+        #
+        # for i in range(10):
+        #     print('Accuracy of %5s : %2d %%' % (
+        #         classes[i], 100 * class_correct[i] / class_total[i]))
+
 
     print('Finished with network {}'.format(net))
 
+    plt.figure(dpi=600)
+    plt.xlabel('iteration')
+    plt.ylabel('Accuracy')
+    plt.plot(iteration_vec,train_accuracy_vec,label='training')
+    plt.plot(iteration_vec,test_accuracy_vec,label='testing')
+    plt.title('{}'.format(net))
+    plt.legend()
+
+
 
 ########################################################################
-# 5. Test the network on the test data
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#
-# We have trained the network for 2 passes over the training dataset.
-# But we need to check if the network has learnt anything at all.
-#
-# We will check this by predicting the class label that the neural network
-# outputs, and checking it against the ground-truth. If the prediction is
-# correct, we add the sample to the list of correct predictions.
-#
-# Okay, first step. Let us display an image from the test set to get familiar.
-# %%
-
-dataiter = iter(testloader)
-images, labels = dataiter.next()
-
-# print images
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-########################################################################
-# Okay, now let us see what the neural network thinks these examples above are:
-# %%
-outputs = net(Variable(images))
-
-########################################################################
-# The outputs are energies for the 10 classes.
-# Higher the energy for a class, the more the network
-# thinks that the image is of the particular class.
-# So, let's get the index of the highest energy:
-_, predicted = torch.max(outputs.data, 1)
-
-print('Predicted: ', ' '.join('%5s' % classes[int(predicted[j].numpy())]
-                              for j in range(4)))
-# %%
-########################################################################
-# The results seem pretty good.
-#
-# Let us look at how the network performs on the whole dataset.
-# %%
-correct = 0
-total = 0
-for data in testloader:
-    images, labels = data
-    outputs = net(Variable(images))
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
-
-########################################################################
-# That looks waaay better than chance, which is 10% accuracy (randomly picking
-# a class out of 10 classes).
-# Seems like the network learnt something.
-#
-# Hmmm, what are the classes that performed well, and the classes that did
-# not perform well:
-
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-for data in testloader:
-    images, labels = data
-    outputs = net(Variable(images))
-    _, predicted = torch.max(outputs.data, 1)
-    c = (predicted == labels).squeeze()
-    for i in range(4):
-        label = labels[i]
-        class_correct[label] += c[i]
-        class_total[label] += 1
-
-
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' % (
-        classes[i], 100 * class_correct[i] / class_total[i]))
-
-########################################################################
-# Okay, so what next?
-#
-# How do we run these neural networks on the GPU?
-#
-# Training on GPU
-# ----------------
-# Just like how you transfer a Tensor on to the GPU, you transfer the neural
-# net onto the GPU.
-# This will recursively go over all modules and convert their parameters and
-# buffers to CUDA tensors:
-#
-# .. code:: python
-#
-#     net.cuda()
-#
-#
-# Remember that you will have to send the inputs and targets at every step
-# to the GPU too:
-#
-# ::
-#
-#         inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-#
-# Why dont I notice MASSIVE speedup compared to CPU? Because your network
-# is realllly small.
-#
-# **Exercise:** Try increasing the width of your network (argument 2 of
-# the first ``nn.Conv2d``, and argument 1 of the second ``nn.Conv2d`` –
-# they need to be the same number), see what kind of speedup you get.
-#
-# **Goals achieved**:
-#
-# - Understanding PyTorch's Tensor library and neural networks at a high level.
-# - Train a small neural network to classify images
-#
-# Training on multiple GPUs
-# -------------------------
-# If you want to see even more MASSIVE speedup using all of your GPUs,
-# please check out :doc:`data_parallel_tutorial`.
-#
-# Where do I go next?
-# -------------------
-#
-# -  :doc:`Train neural nets to play video games </intermediate/reinforcement_q_learning>`
-# -  `Train a state-of-the-art ResNet network on imagenet`_
-# -  `Train an face generator using Generative Adversarial Networks`_
-# -  `Train a word-level language model using Recurrent LSTM networks`_
-# -  `More examples`_
-# -  `More tutorials`_
-# -  `Discuss PyTorch on the Forums`_
-# -  `Chat with other users on Slack`_
-#
-# .. _Train a state-of-the-art ResNet network on imagenet: https://github.com/pytorch/examples/tree/master/imagenet
-# .. _Train an face generator using Generative Adversarial Networks: https://github.com/pytorch/examples/tree/master/dcgan
-# .. _Train a word-level language model using Recurrent LSTM networks: https://github.com/pytorch/examples/tree/master/word_language_model
-# .. _More examples: https://github.com/pytorch/examples
-# .. _More tutorials: https://github.com/pytorch/tutorials
-# .. _Discuss PyTorch on the Forums: https://discuss.pytorch.org/
-# .. _Chat with other users on Slack: http://pytorch.slack.com/messages/beginner/
